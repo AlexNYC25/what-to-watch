d2c759309bf0bd1a80e6fb1e4eec491c963850d9
Alexis Montes <montes.alexis25@gmail.com>
2021-12-22 00:39:39 -0500

    created individual component to display results

---
+++ components/SearchMediaCard.js
@@ -0,0 +1,39 @@
+import Link from 'next/link';
+import {Card, CardMedia, CardContent, Typography } from '@mui/material';
+
+/*
+    props:
+        imagePath: string
+        title: string
+        mediaType: string (movie, tvShow )
+        mediaId: string
+*/
+export default function SearchMediaCard(props) {
+    
+
+    return (
+        <Link href={`/${props.mediaType}/${props.id}`}>
+            <Card
+                className="search-card"
+                sx={{maxWidth: 345}}
+            >
+                <CardMedia
+                    className="search-card-image"
+                    component="img"
+                    image={props.imagePath ? `https://image.tmdb.org/t/p/w500/${props.imagePath}` : 'https://via.placeholder.com/300x450?text=Movie+Not+Found'}
+                    title={props.title}
+                />
+
+                <CardContent className='search-card-text'>
+                    <Typography
+                        varient="body2"
+                        component="p"
+                        color="textSecondary"
+                    >
+                        {props.title}
+                    </Typography>
+                </CardContent>
+            </Card>
+        </Link>
+    );
+}
