8fd213d03792684c4d8d941fd73288c7f4cc86cd
Alexis Montes <montes.alexis25@gmail.com>
2021-12-22 01:05:49 -0500

    imported second stylesheets

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import { UserProvider } from '@auth0/nextjs-auth0'
 import React from 'react';
 import '../styles/globals.css'
+import '../styles/styles.css'
 
 function MyApp({ Component, pageProps }) {
   return (
