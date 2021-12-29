76f51e83b9810e518b1dce3da1292e0b33e7f2ee
Alexis Montes <montes.alexis25@gmail.com>
2021-12-29 00:38:26 -0500

    cleaned up jsx replaced with components

--- pages/topRated.js
+++ pages/topRated.js
@@ -1,21 +1,14 @@
-import { Container, Grid, Card, CardMedia, CardContent, Typography, CardActions, IconButton } from "@mui/material";
+import { Container, Grid } from "@mui/material";
 import Layout from '../components/Layout';
+import TopRatedCard from "../components/TopRatedCard";
 
 import {useState, useEffect} from 'react';
-
-import {useUser} from '@auth0/nextjs-auth0';
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar, faFilm, faTv } from '@fortawesome/free-solid-svg-icons';
-
 import axios from 'axios';
 
 export default function TopRated() {
 
     const [movies, setMovies] = useState([]);
     const [tvShows, setTvShows] = useState([]);
-
-    const { user, error, isLoading } = useUser();
 
     let handleTopRated = async () => {
         let response = await axios.get('/api/topRated');
@@ -33,89 +26,21 @@ export default function TopRated() {
         <Layout>
 
             <Container sx={{mt:10, mb:10}}>
-                <h1>Top Rated</h1>
-                <h2>Top Rated Movies</h2>
+                <h1 className="top-rated-media-heading">Top Rated</h1>
+                <h2 className="top-rated-media-heading">Top Rated Movies</h2>
 
                 <Grid container spacing={2}>
                     {movies.map( item => (
-                        <Grid item xs={6} sm={4} md={3} lg={2} xl={2} key={item.id} style={{display: 'flex'}}>
-                            <Card sx={{maxWidth: 345}} style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}} >
-                                <CardMedia
-                                    component="img"
-                                    image={item.poster_path ? `https://image.tmdb.org/t/p/w185_and_h278_bestv2/${item.poster_path}` : 'https://via.placeholder.com/300x450?text=Movie+Not+Found'}
-                                    style={{height: '100%', width: '100%'}}
-                                    title={item.title ? item.title : item.name}
-                                />
-
-                                <CardContent>
-							        <Typography varient="body5" color="textSecondary" component="p">
-								        {item.title ? item.title : item.name}
-							        </Typography>
-						        </CardContent>
-                            
-                                <CardActions>
-                                    <p>{item.vote_average}</p>
-                                    <IconButton 
-                                        aria-label="add to favorites" 
-                                        disabled={user ? false : true} 
-                                        onClick={() => handleButtonClick(item.id, 'favorites')}
-                                    >
-                                        <FontAwesomeIcon icon={faStar} />
-                                    </IconButton>
-                                    <IconButton 
-                                        aria-label="add to watchlist" 
-                                        disabled={user ? false : true}
-                                        onClick={() => handleButtonClick(item.id, 'watchlist')}
-                                    >
-                                        {item.media_type === 'movie' ? <FontAwesomeIcon icon={faFilm} /> : <FontAwesomeIcon icon={faTv} />}
-                                    </IconButton>
-                                </CardActions>
-
-                            </Card>
-                        </Grid>
+                        <TopRatedCard mediaType="movie" id={item.id} title={item.title} imagePath={item.poster_path} />
                     ))}
 
                 </Grid>
 
-                <h2>Top Rated TV Shows</h2>
+                <h2 className="top-rated-media-heading">Top Rated TV Shows</h2>
                 <Grid container spacing={2}>
                     {tvShows.map( item => (
-                        <Grid item xs={6} sm={4} md={3} lg={2} xl={2} key={item.id} style={{display: 'flex'}}>
-                            <Card sx={{maxWidth: 345}} style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}} >
-                                <CardMedia
-                                    component="img"
-                                    image={item.poster_path ? `https://image.tmdb.org/t/p/w185_and_h278_bestv2/${item.poster_path}` : 'https://via.placeholder.com/300x450?text=Movie+Not+Found'}
-                                    style={{height: '100%', width: '100%'}}
-                                    title={item.title ? item.title : item.name}
-                                />
-
-                                <CardContent>
-                                    <Typography varient="body5" color="textSecondary" component="p">
-                                        {item.title ? item.title : item.name}
-                                    </Typography>
-                                </CardContent>
-
-                                <CardActions>
-                                    <p>{item.vote_average}</p>
-                                    <IconButton
-                                        aria-label="add to favorites"
-                                        disabled={user ? false : true}
-                                        onClick={() => handleButtonClick(item.id, 'favorites')}
-                                    >
-                                        <FontAwesomeIcon icon={faStar} />
-                                    </IconButton>
-
-                                    <IconButton
-                                        aria-label="add to watchlist"
-                                        disabled={user ? false : true}
-                                        onClick={() => handleButtonClick(item.id, 'watchlist')}
-                                    >
-                                        {item.media_type === 'movie' ? <FontAwesomeIcon icon={faFilm} /> : <FontAwesomeIcon icon={faTv} />}
-                                    </IconButton>
-                                </CardActions>
-
-                            </Card>
-                        </Grid>
+                        
+                        <TopRatedCard mediaType="tvShow" id={item.id} title={item.name} imagePath={item.poster_path} />
                     ))}
                 </Grid>
 
