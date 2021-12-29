f35efe41cc9bf74e10bf18d4ca21d07fd3ce40d6
Alexis Montes <montes.alexis25@gmail.com>
2021-12-29 00:34:35 -0500

    cleaned up jsx replaced with components

--- pages/discover.js
+++ pages/discover.js
@@ -5,11 +5,8 @@ import { useEffect, useState } from "react";
 
 import { useUser } from '@auth0/nextjs-auth0';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar, faFilm, faTv } from '@fortawesome/free-solid-svg-icons';
-
-
 import axios from "axios";
+import DiscoverCard from "../components/DiscoverCard";
 
 export default function Discover() {
 
@@ -62,85 +59,18 @@ export default function Discover() {
 	<Layout>
 		
 		<Container sx={{mt:10, mb:5}}>
-			<h1>Discover</h1>
-			<h2>Trending Movies</h2>
+			<h1 className="discover-media-heading">Discover</h1>
+			<h2 className="discover-media-heading">Trending Movies</h2>
 			<Grid container spacing={2}>
 				{movies.map(item => (
-					<Grid item xs={6} sm={4} md={3} lg={2} xl={2} key={item.id} style={{display: 'flex'}}> 
-					  <Card sx={{maxWidth: 345}} style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}}>
-						<CardMedia
-							component="img"
-							image={item.poster_path ? `https://image.tmdb.org/t/p/w500/${item.poster_path}` : 'https://via.placeholder.com/300x450?text=Movie+Not+Found'}
-							style={{height: '100%', width: '100%'}}
-							title={item.title ? item.title : item.name}
-						/>
-
-						<CardContent>
-							<Typography varient="body5" color="textSecondary" component="p">
-								{item.title ? item.title : item.name}
-							</Typography>
-						</CardContent>
-
-						<CardActions>
-							<p>{item.vote_average}</p>
-							<IconButton 
-                                    aria-label="add to favorites" 
-                                    disabled={user ? false : true} 
-                                    onClick={() => handleButtonClick(item.id, 'favorites')}
-                                >
-                                    <FontAwesomeIcon icon={faStar} />
-                                </IconButton>
-                                <IconButton 
-                                    aria-label="add to watchlist" 
-                                    disabled={user ? false : true}
-                                    onClick={() => handleButtonClick(item.id, 'watchlist')}
-                                >
-                                    {item.media_type === 'movie' ? <FontAwesomeIcon icon={faFilm} /> : <FontAwesomeIcon icon={faTv} />}
-                        		</IconButton>
-						</CardActions>
-						
-					  </Card>
-					</Grid>
+					<DiscoverCard mediaType="movie" title={item.title ? item.title : item.name} id={item.id} imagePath={item.poster_path} />
 				))}
 			</Grid>
-			<h2>Trending Tv Shows</h2>
+			<h2 className="discover-media-heading">Trending Tv Shows</h2>
 			<Grid container spacing={2}>
 				
 				{tvShows.map(item => (
-					<Grid item xs={6} sm={4} md={3} lg={2} xl={2} key={item.id} style={{display: 'flex'}}>
-						<Card sx={{maxWidth: 345}} style={{display: 'flex', flexDirection: 'column', justifyContent: 'space-between'}} >
-							<CardMedia
-								component="img"
-								image={item.poster_path ? `https://image.tmdb.org/t/p/w500/${item.poster_path}` : 'https://via.placeholder.com/300x450?text=Movie+Not+Found'}
-								style={{height: '100%', width: '100%'}}
-								title={item.title ? item.title : item.name}
-							/>
-
-							<CardContent>
-								<Typography varient="body5" color="textSecondary" component="p">
-									{item.title ? item.title : item.name}
-								</Typography>
-							</CardContent>
-
-							<CardActions>
-								<p>{item.vote_average}</p>
-								<IconButton 
-									aria-label="add to favorites" 
-									disabled={user ? false : true}
-									onClick={() => handleButtonClick(item.id, 'favorites')}
-								>
-									<FontAwesomeIcon icon={faStar} />
-								</IconButton>
-								<IconButton 	
-									aria-label="add to watchlist" 
-									disabled={user ? false : true}
-									onClick={() => handleButtonClick(item.id, 'watchlist')}
-								>
-									{item.media_type === 'movie' ? <FontAwesomeIcon icon={faFilm} /> : <FontAwesomeIcon icon={faTv} />}
-								</IconButton>
-							</CardActions>
-						</Card>
-					</Grid>	
+					<DiscoverCard mediaType="tvShow" title={item.title ? item.title : item.name} id={item.id} imagePath={item.poster_path} />
 				))}
 			</Grid>
 		</Container>
