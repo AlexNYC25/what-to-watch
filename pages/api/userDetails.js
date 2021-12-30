19f69a74c41aa553ec534446f001b4809aa42f11
Alexis Montes <montes.alexis25@gmail.com>
2021-12-30 02:41:42 -0500

    fixed stalling issue, error handling

--- pages/api/userDetails.js
+++ pages/api/userDetails.js
@@ -8,36 +8,46 @@ import { withApiAuthRequired, getSession } from "@auth0/nextjs-auth0";
 dbConnect();
 
 export default withApiAuthRequired( async (req, res) => {
-    if(req.method === "GET") {
-        const session = getSession(req, res)
 
+    return new Promise((resolve, reject) => {
+        if(req.method === "GET") {
+            const session = getSession(req, res)
 
-        user.findOne({
-            email: session.user.email
-        }).then(user => {
-
-            collectMediaDetails({
-                tvShowWatchlist: user.tvShowWatchlist,
-                movieWatchlist: user.movieWatchlist,
-                favoriteMovies: user.favoriteMovies,
-                favoriteTvShows: user.favoriteTvShows
-            }).then(userDetails => {
-                console.log(userDetails)
-
-                res.status(200).json({
-                    tvShowWatchlist: userDetails.tvShowWatchlist,
-                    movieWatchlist: userDetails.movieWatchlist,
-                    favoriteMovies: userDetails.favoriteMovies,
-                    favoriteTvShows: userDetails.favoriteTvShows
-                });
+            user.findOne({
+                email: session.user.email
+            }).then(user => {
+                collectMediaDetails({
+                    tvShowWatchlist: user.tvShowWatchlist,
+                    movieWatchlist: user.movieWatchlist,
+                    favoriteMovies: user.favoriteMovies,
+                    favoriteTvShows: user.favoriteTvShows
+                }).then(userDetails => {
+                    res.status(200).json({
+                        tvShowWatchlist: userDetails.tvShowWatchlist,
+                        movieWatchlist: userDetails.movieWatchlist,
+                        favoriteMovies: userDetails.favoriteMovies,
+                        favoriteTvShows: userDetails.favoriteTvShows
+                    });
+                    resolve();
+                }).catch(err => {
+                    res.status(500).json({
+                        error: err,
+                        message: "Error getting user details"
+                    });
+                    reject();
+                })
             }).catch(err => {
-                console.log(err)
+                res.status(500).json({
+                    error: err,
+                    message: "Error getting user details"
+                });
+                reject();
             })
+                                                            
+        } else {
+            return res.status(400).json({'message': 'Method not allowed'});
+        }
+    });
 
-            
-        })
-                                                        
-    } else {
-        res.status(400).json({'message': 'invalid request'});
-    }
+
 });
