dffcc62638672ee47f40649799c917e33bb9b591
Alexis Montes <montes.alexis25@gmail.com>
2021-12-30 02:52:04 -0500

    added documentation, fixed db query

--- pages/api/removeFavorite/tvShow.js
+++ pages/api/removeFavorite/tvShow.js
@@ -4,39 +4,64 @@ import dbConnect from "../../../util/dbConnect";
 
 import User from "../../../models/User";
 
+/*
+    POST /api/removeFavorite/tvShow
+    Body:
+        - mediaId
+    Response:
+        - status code
+        - error
+        - message
+    Note: Route will be protected by Auth0, user is required to be logged in, user details will be retrieved from Auth0
+*/
 export default withApiAuthRequired(async function removeTvShow (req, res) {
-    if (req.method !== "POST") {
-        return res.status(405).end();
-    }
 
-    await dbConnect();
+    return new Promise((resolve, reject) => {
+        if (req.method !== "POST") {
+            return res.status(405).json({ message: "Method not allowed" });
+        }
     
-    const mediaId = req.body.mediaId;
+        await dbConnect();
+        
+        const mediaId = req.body.mediaId;
+    
+        const session = getSession(req, res);
+        const userId = session.user.name;
+    
+        User.find({
+            email: userId
+        }).then(user => {
 
-    const session = getSession(req, res);
-    const userId = session.user.name;
+            if (!user.favoriteTvShows) {
+                user.favoriteTvShows = [];
+            }
 
-    User.findOne({
-        where: {
-            id: userId
-        }
-    }).then(user => {
-
-        if(user.favoriteTvShows.includes(mediaId)) {
-            user.favoriteTvShows = user.favoriteTvShows.filter(id => id !== mediaId);
-        }
-
-        user.save().then(() => {
-            res.status(200).json({message: "Tv show removed from favorites"});
+            // check if the passed mediaId is in the user's favorites
+            if(user.favoriteTvShows.includes(mediaId)) {
+                user.favoriteTvShows = user.favoriteTvShows.filter(id => id !== mediaId);
+            } else {
+                return res.status(400).json({
+                    message: "The passed TV Show id is not in the user's favorites"
+                });
+            }
+    
+            user.save().then(() => {
+                res.status(200).json({message: "Tv show removed from favorites"});
+                resolve();
+            })
+            .catch(err => {
+                res.status(500).json({error: err, message: "Error saving user"});
+                reject();
+            });
+    
         })
         .catch(err => {
-            res.status(500).json({message: "Something went wrong"});
+            res.status(500).json({error: err, message: "Error querying user database"});
+            reject();
         });
-
     })
-    .catch(err => {
-        res.status(500).json({message: "Something went wrong"});
-    });
+
+
 
 
 })
