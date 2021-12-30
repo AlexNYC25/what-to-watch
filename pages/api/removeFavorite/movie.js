5f41e3c90d2c0048da552f168814197d5153f9f5
Alexis Montes <montes.alexis25@gmail.com>
2021-12-30 02:47:15 -0500

    fixed documentation, response stalling

--- pages/api/removeFavorite/movie.js
+++ pages/api/removeFavorite/movie.js
@@ -4,40 +4,65 @@ import dbConnect from "../../../util/dbConnect";
 
 import User from "../../../models/User";
 
+/*
+    POST /api/removeFavorite/movie
+    Body:
+        - mediaId
+    Response:
+        - status code
+        - message
+    Note: Route will be protected by Auth0, user is required to be logged in, user details will be retrieved from Auth0
+*/
 export default withApiAuthRequired(async function movie(req, res) {
-    if(req.method !== "POST") {
-        res.status(405).end();
-        return;
-    }
 
-    await dbConnect();
+    return new Promise((resolve, reject) => {
 
-    const mediaId = req.body.mediaId;
-
-    const session = getSession(req, res);
-    const userId  = session.user.name;
-
-    User.findOne({
-        where: {
-            id: userId
+        // allow only POST requests
+        if(req.method !== "POST") {
+            return res.status(405).json({message: "Method not allowed"});
         }
-    })
-    .then(user => {
+    
+        await dbConnect();
+    
+        const mediaId = req.body.mediaId;
+    
+        const session = getSession(req, res);
+        const userId  = session.user.name;
+    
+        User.find({
+            email: userId        
+        })
+        .then(user => {
 
-        if(user.favoriteMovies.includes(mediaId)) {
-            user.favoriteMovies = user.favoriteMovies.filter(id => id !== mediaId);
-        }
+            // create empty array if favoriteMovies dosent exist
+            if(!user.favoriteMovies){
+                user.favoriteMovies = []
+            }
 
-        user.save().then(() => {
-            res.status(200).json({message: "Movie removed from favorites"});
+            // check if the passed mediaId is in the user's favorites
+            if(user.favoriteMovies.includes(mediaId)) {
+                user.favoriteMovies = user.favoriteMovies.filter(id => id !== mediaId);
+            } else {
+                return res.status(400).json({
+                    message: "The passed movie Id is not in the user's favorites"
+                });
+            }
+    
+            user.save().then(() => {
+                res.status(200).json({message: "Movie removed from favorites"});
+                resolve();
+            })
+            .catch(err => {
+                res.status(500).json({error: err, message: "Error saving user"});
+                reject();
+            })
+    
         })
         .catch(err => {
-            res.status(500).json({message: "Something went wrong"});
+            res.status(500).json({error: err, message: "Error querying user database"});
+            reject();
         })
+    });
 
-    })
-    .catch(err => {
-        res.status(500).json({message: "Something went wrong"});
-    })
 
 })
